752c6c89a50867986e7d95b488f54741b665433a
nguyentinh0310 <53959609+nguyentinh0310@users.noreply.github.com>
2021-10-10 19:32:31 +0700

    Delete server directory

--- server/router/coversation.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const conversationCtrl = require('../controllers/conversationCtrl');
-const authenticatedMiddleware = require('../middlewares/auth');
-
-router.post('/',authenticatedMiddleware, conversationCtrl.newConversation);
-
-router.get('/:userId', authenticatedMiddleware,conversationCtrl.getConversation);
-
-module.exports = router;
