19b72e095ca418341edea3613bf7425e19dc5579
Your Name <nguyenquoctinh0310@gmail.com>
2021-10-11 15:42:45 +0700

    ok

--- client/src/components/routing/ProtectedRoute.jsx
+++ client/src/components/routing/ProtectedRoute.jsx
@@ -1,31 +1,28 @@
-import Loader from 'components/layout/Loader'
-import { useContext } from 'react'
-import { Redirect, Route } from 'react-router-dom'
-import { AuthContext } from '../../api/contexts/AuthContext'
+import Loader from 'components/layout/Loader';
+import { useContext } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { AuthContext } from '../../api/contexts/AuthContext';
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-	const {
-		authState: { loading, isAuthenticated }
-	} = useContext(AuthContext)
+  const {
+    authState: { loading, isAuthenticated },
+  } = useContext(AuthContext);
 
-	if (loading)
-		return (
-            <Loader/>
-		)
+  if (loading) return <Loader />;
 
-	return (
-		<Route
-			{...rest}
-			render={props =>
-				isAuthenticated ? (
-					<>
-						<Component {...rest} {...props} />
-					</>
-				) : (
-					<Redirect to='/login' />
-				)
-			}
-		/>
-	)
-}
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <>
+            <Component {...rest} {...props} />
+          </>
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
