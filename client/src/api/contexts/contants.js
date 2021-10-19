4596b4bd762723027678c4858a66f0f734518ac9
nguyentinh0310 <nguyenquoctinh0310@gmail.com>
2021-10-19 08:40:04 +0700

    ok

--- client/src/api/contexts/contants.js
+++
@@ -1,5 +0,0 @@
-export const LOCAL_STORAGE_TOKEN_NAME = 'userChat';
-
-export const URL_SOCKET = process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:9000'
-    : 'https://chat-chill.herokuapp.com';
